03377f89887c2aca3d1e16a35d985c0f32c0c53c
oldtree <435594427@qq.com>
2020-12-15 17:31:23 +0800

    code server is ok

--- github.com/grapery/grapery/api/service/service.pb.gw.go
+++ github.com/grapery/grapery/api/service/service.pb.gw.go
@@ -13,14 +13,14 @@ import (
 	"io"
 	"net/http"
 
-	"github.com/golang/protobuf/descriptor"
-	"github.com/golang/protobuf/proto"
-	"github.com/grpc-ecosystem/grpc-gateway/runtime"
-	"github.com/grpc-ecosystem/grpc-gateway/utilities"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/utilities"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/grpclog"
+	"google.golang.org/grpc/metadata"
 	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/proto"
 )
 
 // Suppress "imported and not used" errors
@@ -29,7 +29,7 @@ var _ io.Reader
 var _ status.Status
 var _ = runtime.String
 var _ = utilities.NewDoubleArray
-var _ = descriptor.ForMessage
+var _ = metadata.Join
 
 func request_Grapery_PingPong_0(ctx context.Context, marshaler runtime.Marshaler, client GraperyClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq Ping
@@ -136,18 +136,22 @@ func local_request_Grapery_Logout_0(ctx context.Context, marshaler runtime.Marsh
 // RegisterGraperyHandlerServer registers the http handlers for service Grapery to "mux".
 // UnaryRPC     :call GraperyServer directly.
 // StreamingRPC :currently unsupported pending https://github.com/grpc/grpc-go/issues/906.
+// Note that using this registration option will cause many gRPC library features to stop working. Consider using RegisterGraperyHandlerFromEndpoint instead.
 func RegisterGraperyHandlerServer(ctx context.Context, mux *runtime.ServeMux, server GraperyServer) error {
 
 	mux.Handle("POST", pattern_Grapery_PingPong_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
+		var stream runtime.ServerTransportStream
+		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		rctx, err := runtime.AnnotateIncomingContext(ctx, mux, req)
+		rctx, err := runtime.AnnotateIncomingContext(ctx, mux, req, "/grapery.api.Grapery/PingPong")
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
 		}
 		resp, md, err := local_request_Grapery_PingPong_0(rctx, inboundMarshaler, server, req, pathParams)
+		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
 		ctx = runtime.NewServerMetadataContext(ctx, md)
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
@@ -161,13 +165,16 @@ func RegisterGraperyHandlerServer(ctx context.Context, mux *runtime.ServeMux, se
 	mux.Handle("POST", pattern_Grapery_Login_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
+		var stream runtime.ServerTransportStream
+		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		rctx, err := runtime.AnnotateIncomingContext(ctx, mux, req)
+		rctx, err := runtime.AnnotateIncomingContext(ctx, mux, req, "/grapery.api.Grapery/Login")
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
 		}
 		resp, md, err := local_request_Grapery_Login_0(rctx, inboundMarshaler, server, req, pathParams)
+		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
 		ctx = runtime.NewServerMetadataContext(ctx, md)
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
@@ -181,13 +188,16 @@ func RegisterGraperyHandlerServer(ctx context.Context, mux *runtime.ServeMux, se
 	mux.Handle("POST", pattern_Grapery_Logout_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
+		var stream runtime.ServerTransportStream
+		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		rctx, err := runtime.AnnotateIncomingContext(ctx, mux, req)
+		rctx, err := runtime.AnnotateIncomingContext(ctx, mux, req, "/grapery.api.Grapery/Logout")
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
 		}
 		resp, md, err := local_request_Grapery_Logout_0(rctx, inboundMarshaler, server, req, pathParams)
+		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
 		ctx = runtime.NewServerMetadataContext(ctx, md)
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
@@ -243,7 +253,7 @@ func RegisterGraperyHandlerClient(ctx context.Context, mux *runtime.ServeMux, cl
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		rctx, err := runtime.AnnotateContext(ctx, mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req, "/grapery.api.Grapery/PingPong")
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
@@ -263,7 +273,7 @@ func RegisterGraperyHandlerClient(ctx context.Context, mux *runtime.ServeMux, cl
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		rctx, err := runtime.AnnotateContext(ctx, mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req, "/grapery.api.Grapery/Login")
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
@@ -283,7 +293,7 @@ func RegisterGraperyHandlerClient(ctx context.Context, mux *runtime.ServeMux, cl
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		rctx, err := runtime.AnnotateContext(ctx, mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req, "/grapery.api.Grapery/Logout")
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
@@ -303,11 +313,11 @@ func RegisterGraperyHandlerClient(ctx context.Context, mux *runtime.ServeMux, cl
 }
 
 var (
-	pattern_Grapery_PingPong_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "example", "ping"}, "", runtime.AssumeColonVerbOpt(true)))
+	pattern_Grapery_PingPong_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "example", "ping"}, ""))
 
-	pattern_Grapery_Login_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "login"}, "", runtime.AssumeColonVerbOpt(true)))
+	pattern_Grapery_Login_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "login"}, ""))
 
-	pattern_Grapery_Logout_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "login"}, "", runtime.AssumeColonVerbOpt(true)))
+	pattern_Grapery_Logout_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "login"}, ""))
 )
 
 var (
--- regen.sh
+++ regen.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
 go env -w GO111MODULE=on
+export PATH="/home/ubuntu/work/bin":$PATH
 
-if [ -z "$PROTOC_INSTALL" ]; then
-	echo "PROTOC_INSTALL not set"
-	exit 1
-fi
-
-GOPATH=/home/ubuntu/work
-basepath=$GOPATH/src
+IncludePath=/snap/protobuf/current/include
+GPATH=/home/ubuntu/work
+basepath=$GPATH/src
 pb_package=github.com/grapery/grapery/pb
 proto_install="$PROTOC_INSTALL"
 go_package=api
@@ -17,19 +14,19 @@ rm -rf $go_package
 for i in $(ls $basepath/$pb_package/*.proto); do
 	echo $i
 	fn=$pb_package/$(basename "$i")
-	protoc -I$proto_install/include -I. \
-		-I$GOPATH/src \
-		-I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/\
-		-I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis\
+	protoc -I$IncludePath -I. \
+		-I$GPATH/src \
+		-I$GPATH/src/github.com/grpc-ecosystem/grpc-gateway/\
+		-I$GPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis\
 		--go_out=plugins=grpc:. "$fn"
-	protoc -I$proto_install/include -I. \
-		-I$GOPATH/src \
-		-I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/\
-		-I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis\
+	protoc -I$IncludePath -I. \
+		-I$GPATH/src \
+		-I$GPATH/src/github.com/grpc-ecosystem/grpc-gateway/\
+		-I$GPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis\
 		--grpc-gateway_out=logtostderr=true:. "$fn"
-	protoc -I$proto_install/include -I. \
-		-I$GOPATH/src \
-		-I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/\
-		-I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis\
+	protoc -I$IncludePath -I. \
+		-I$GPATH/src \
+		-I$GPATH/src/github.com/grpc-ecosystem/grpc-gateway/\
+		-I$GPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis\
 		--swagger_out=logtostderr=true:. "$fn"
 done
