d1becadcb7cb22496d023139753c4eaf506eaecc
grapeVine <treeui.old@gmail.com>
2021-01-16 13:14:12 +0800

    shoot

--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/go-redis/redis v6.15.6+incompatible
 	github.com/go-sql-driver/mysql v1.4.1
 	github.com/golang/protobuf v1.4.3
+	github.com/grpc-ecosystem/grpc-gateway v1.16.0
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.0.1
 	github.com/jinzhu/gorm v1.9.8
 	github.com/kr/pretty v0.2.1 // indirect
---
+++ regmac.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if [ -z "$PROTOC_INSTALL" ]; then
+	echo "PROTOC_INSTALL not set"
+	exit 1
+fi
+
+basepath=$GOPATH/src
+pb_package=github.com/grapery/grapery/pb
+proto_install="$PROTOC_INSTALL"
+go_package=api
+rm -rf $go_package
+
+cd $basepath
+for i in $(ls $basepath/$pb_package/*.proto); do
+	echo $i
+	fn=$pb_package/$(basename "$i")
+	protoc -I$proto_install/include -I. \
+		-I$GOPATH/src \
+		-I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/\
+		-I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis\
+		--go_out=plugins=grpc:. "$fn"
+	protoc -I$proto_install/include -I. \
+		-I$GOPATH/src \
+		-I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/\
+		-I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis\
+		--grpc-gateway_out=logtostderr=true:. "$fn"
+	protoc -I$proto_install/include -I. \
+		-I$GOPATH/src \
+		-I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/\
+		-I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis\
+		--swagger_out=logtostderr=true:. "$fn"
+done
