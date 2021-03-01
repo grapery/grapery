9ed6d4ca54e7987dbe42b22bb833a66f5359e461
oldtree <435594427@qq.com>
2021-03-01 18:30:33 +0800

    update impl

--- pkg/auth/auth.go
+++ pkg/auth/auth.go
@@ -5,6 +5,13 @@ import (
 	_ "github.com/gin-contrib/sessions/redis"
 )
 
+type AuthServicer interface {
+	Register(uid uint64) error
+	Login(uid uint64) error
+	Logout(uid uint64) error
+	ResetPassword(uid uint64, newPwd, oldPwd string) error
+}
+
 // auth service
 type AuthService struct {
 }
