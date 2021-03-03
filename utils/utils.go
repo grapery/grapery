66dd7f38e1d362bec7abc23fc811e7904c76733b
oldtree <435594427@qq.com>
2021-03-03 19:05:59 +0800

    update handle

--- utils/utils.go
+++ utils/utils.go
@@ -1,16 +1,34 @@
 package utils
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
 
-type Context struct {
-	*gin.Context
-	UserID    uint64
-	GroupID   uint64
-	ProjectID uint64
-}
+	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
+)
+
+const (
+	CookieName = "grapery"
+
+	UserKey    = "_userid"
+	GroupKey   = "_groupid"
+	ProjectKey = "_projectid"
+	ErrKey     = "_err"
+	RespKey    = "_resp"
+)
 
 func WrapHandler(h gin.HandlerFunc) gin.HandlerFunc {
 	return func(c *gin.Context) {
+		cookie, err := c.Cookie(CookieName)
+		if err != nil {
 
+		}
+		log.Info("cookie name: ", cookie)
+		h(c)
+		if c.Keys[ErrKey] != nil {
+			c.JSON(http.StatusOK, c.Keys[ErrKey])
+			return
+		}
+		c.JSON(http.StatusOK, c.Keys[RespKey])
 	}
 }
