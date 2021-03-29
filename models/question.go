1701eb46e1ef32ec8e0003421d93bb85a8c42b4f
oldtree <435594427@qq.com>
2021-03-29 19:01:21 +0800

    fix issues

--- models/question.go
+++ models/question.go
@@ -3,14 +3,19 @@ package models
 // base on project
 type Question struct {
 	IDBase
-	UserID      uint64 `json:"user_id,omitempty"`
-	GroupID     uint64 `json:"group_id,omitempty"`
-	ProjectID   uint64 `json:"project_id,omitempty"`
-	Tital       string `json:"tital,omitempty"`
-	Description string `json:"description,omitempty"`
-	Tags        string `json:"tags,omitempty"`
+	UserID    uint64 `json:"user_id,omitempty"`
+	GroupID   uint64 `json:"group_id,omitempty"`
+	ProjectID uint64 `json:"project_id,omitempty"`
+	Tital     string `json:"tital,omitempty"`
+	Content   string `json:"description,omitempty"`
+	Tags      uint64 `json:"tags,omitempty"`
+	RefId     uint64 `json:"ref_id,omitempty"`
 }
 
 func (q Question) TableName() string {
 	return "question"
+}
+
+func GetProjectQuestions(projectID int) (list []*Question, err error) {
+	return nil, nil
 }
