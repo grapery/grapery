1ccc21e725319db14f11bfa53af5a327c112e522
grapeVine <treeui.old@gmail.com>
2023-03-25 22:10:15 +0800

    add open api

--- service/openai/openai.go
+++ service/openai/openai.go
@@ -1,1 +1,155 @@
 package openai
+
+import (
+	"bytes"
+	"context"
+	"encoding/base64"
+	"errors"
+	"fmt"
+	"image/png"
+	"io"
+	"os"
+
+	"github.com/sashabaranov/go-openai"
+)
+
+const (
+	OpenAI_Token = ""
+)
+
+type OpenAIClient struct {
+	Clent *openai.Client
+}
+
+func (c *OpenAIClient) ChatOneOnOne(ctx context.Context) {
+	req := openai.CompletionRequest{
+		Model:     openai.GPT3Ada,
+		MaxTokens: 5,
+		Prompt:    "Lorem ipsum",
+	}
+	resp, err := c.Clent.CreateCompletion(ctx, req)
+	if err != nil {
+		fmt.Printf("Completion error: %v\n", err)
+		return
+	}
+	fmt.Println(resp.Choices[0].Text)
+}
+
+func (c *OpenAIClient) ChatStream(ctx context.Context) {
+	req := openai.CompletionRequest{
+		Model:     openai.GPT3Ada,
+		MaxTokens: 5,
+		Prompt:    "Lorem ipsum",
+		Stream:    true,
+	}
+	stream, err := c.Clent.CreateCompletionStream(ctx, req)
+	if err != nil {
+		fmt.Printf("CompletionStream error: %v\n", err)
+		return
+	}
+	defer stream.Close()
+
+	for {
+		response, err := stream.Recv()
+		if errors.Is(err, io.EOF) {
+			fmt.Println("Stream finished")
+			return
+		}
+
+		if err != nil {
+			fmt.Printf("Stream error: %v\n", err)
+			return
+		}
+
+		fmt.Printf("Stream response: %v\n", response)
+	}
+}
+
+func (c *OpenAIClient) TextGen(ctx context.Context) {
+	req := openai.CompletionRequest{
+		Model:     openai.GPT3Ada,
+		MaxTokens: 5,
+		Prompt:    "Lorem ipsum",
+	}
+	resp, err := c.Clent.CreateCompletion(ctx, req)
+	if err != nil {
+		fmt.Printf("Completion error: %v\n", err)
+		return
+	}
+	fmt.Println(resp.Choices[0].Text)
+}
+
+func (c *OpenAIClient) ImageGen(ctx context.Context) {
+	// Sample image by link
+	reqUrl := openai.ImageRequest{
+		Prompt:         "Parrot on a skateboard performs a trick, cartoon style, natural light, high detail",
+		Size:           openai.CreateImageSize256x256,
+		ResponseFormat: openai.CreateImageResponseFormatURL,
+		N:              1,
+	}
+
+	respUrl, err := c.Clent.CreateImage(ctx, reqUrl)
+	if err != nil {
+		fmt.Printf("Image creation error: %v\n", err)
+		return
+	}
+	fmt.Println(respUrl.Data[0].URL)
+
+	// Example image as base64
+	reqBase64 := openai.ImageRequest{
+		Prompt:         "Portrait of a humanoid parrot in a classic costume, high detail, realistic light, unreal engine",
+		Size:           openai.CreateImageSize256x256,
+		ResponseFormat: openai.CreateImageResponseFormatB64JSON,
+		N:              1,
+	}
+
+	respBase64, err := c.Clent.CreateImage(ctx, reqBase64)
+	if err != nil {
+		fmt.Printf("Image creation error: %v\n", err)
+		return
+	}
+
+	imgBytes, err := base64.StdEncoding.DecodeString(respBase64.Data[0].B64JSON)
+	if err != nil {
+		fmt.Printf("Base64 decode error: %v\n", err)
+		return
+	}
+
+	r := bytes.NewReader(imgBytes)
+	imgData, err := png.Decode(r)
+	if err != nil {
+		fmt.Printf("PNG decode error: %v\n", err)
+		return
+	}
+
+	file, err := os.Create("example.png")
+	if err != nil {
+		fmt.Printf("File creation error: %v\n", err)
+		return
+	}
+	defer file.Close()
+
+	if err := png.Encode(file, imgData); err != nil {
+		fmt.Printf("PNG encode error: %v\n", err)
+		return
+	}
+
+	fmt.Println("The image was saved as example.png")
+}
+
+func (c *OpenAIClient) SpeedToText(ctx context.Context) {
+	req := openai.AudioRequest{
+		Model:    openai.Whisper1,
+		FilePath: "recording.mp3",
+	}
+	resp, err := c.Clent.CreateTranscription(ctx, req)
+	if err != nil {
+		fmt.Printf("Transcription error: %v\n", err)
+		return
+	}
+	fmt.Println(resp.Text)
+}
+
+func (c *OpenAIClient) TrainModel(ctx context.Context) {
+
+}
