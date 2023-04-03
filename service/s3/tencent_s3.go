64ada3e035a22d7c3717b8f9385f3f9fd6b1ab6d
grapeVine <treeui.old@gmail.com>
2023-04-03 18:15:07 +0800

    implement s3

--- service/s3/tencent_s3.go
+++ service/s3/tencent_s3.go
@@ -14,28 +14,53 @@ import (
 /*avator,jpg,png,content*/
 
 const (
-	DefaultBucket = "grapery-1301865260"
-	SECRETID      = ""
-	SecretKey     = ""
+	DefaultBucket       = "grapery-1301865260"
+	DefaultBucketUrl    = "https://grapery-1301865260.cos.ap-shanghai.myqcloud.com"
+	DefaultServiceQuery = "service.cos.myqcloud.com"
+	SECRETID            = ""
+	SecretKey           = ""
 )
+
+type ResourceType int
+
+const (
+	ResourceTypeAvator  ResourceType = 1
+	ResourceTypeContent ResourceType = 2
+	ResourceTypeMusic   ResourceType = 3
+	ResourceTypeImage   ResourceType = 4
+	ResourceTypeVideo   ResourceType = 5
+)
+
+func (r ResourceType) String() string {
+	switch r {
+	case ResourceTypeAvator:
+		return "avator"
+	case ResourceTypeContent:
+		return "content"
+	case ResourceTypeMusic:
+		return "music"
+	case ResourceTypeImage:
+		return "image"
+	case ResourceTypeVideo:
+		return "video"
+	}
+	return ""
+}
 
 type S3Client struct {
 	TencentS3Client *cos.Client
 }
 
 func NewS3Client() *S3Client {
-	// 将 examplebucket-1250000000 和 COS_REGION 修改为用户真实的信息
-	// 存储桶名称，由 bucketname-appid 组成，appid 必须填入，可以在 COS 控制台查看存储桶名称。https://console.cloud.tencent.com/cos5/bucket
-	// COS_REGION 可以在控制台查看，https://console.cloud.tencent.com/cos5/bucket, 关于地域的详情见 https://cloud.tencent.com/document/product/436/6224
-	u, _ := url.Parse("https://examplebucket-1250000000.cos.COS_REGION.myqcloud.com")
-	// 用于 Get Service 查询，默认全地域 service.cos.myqcloud.com
-	su, _ := url.Parse("https://cos.COS_REGION.myqcloud.com")
+	u, _ := url.Parse(DefaultBucketUrl)
+	su, _ := url.Parse(DefaultServiceQuery)
 	b := &cos.BaseURL{BucketURL: u, ServiceURL: su}
-	// 1.永久密钥
 	client := cos.NewClient(b, &http.Client{
 		Transport: &cos.AuthorizationTransport{
-			SecretID:  os.Getenv("SECRETID"),  // 用户的 SecretId，建议使用子账号密钥，授权遵循最小权限指引，降低使用风险。子账号密钥获取可参考 https://cloud.tencent.com/document/product/598/37140
-			SecretKey: os.Getenv("SECRETKEY"), // 用户的 SecretKey，建议使用子账号密钥，授权遵循最小权限指引，降低使用风险。子账号密钥获取可参考 https://cloud.tencent.com/document/product/598/37140
+			SecretID: os.Getenv("SECRETID"),
+			// 用户的 SecretId，建议使用子账号密钥，授权遵循最小权限指引，降低使用风险。子账号密钥获取可参考 https://cloud.tencent.com/document/product/598/37140
+			SecretKey: os.Getenv("SECRETKEY"),
+			// 用户的 SecretKey，建议使用子账号密钥，授权遵循最小权限指引，降低使用风险。子账号密钥获取可参考 https://cloud.tencent.com/document/product/598/37140
 		},
 	})
 	return &S3Client{
